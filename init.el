934a6e35db276df6328622ed84b32420c6a889bf
Marco Orovecchia <marco.orovecchia@automic.com>
2016-08-18 15:14:50 +0200

    added gnus layer

--- init.el
+++ init.el
@@ -51,6 +51,7 @@ values."
      version-control
      windows-scripts
      yaml
+     gnus
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
