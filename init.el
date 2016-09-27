cbe0623decf4f6bd8220dd9d667c8d4a8fb6e543
Marco Orovecchia <marco.orovecchia@automic.com>
2016-09-27 12:54:02 +0200

    Dont create lockfiles

--- init.el
+++ init.el
@@ -277,6 +277,7 @@ This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
   (setq projectile-indexing-method 'native)
+  (setq create-lockfiles nil)
   (custom-set-variables
     '(ispell-dictionary "british")
     (when (eq system-type 'windows-nt)
