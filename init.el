db87b8ba470272c03e77dc27514ded58afa8609e
Marco Orovecchia <marco.orovecchia@automic.com>
2016-12-19 15:12:31 +0100

    changed theme

--- init.el
+++ init.el
@@ -156,14 +156,14 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(cyberpunk
+   dotspacemacs-themes '(alect-black-alt
+                         cyberpunk
                          ample-zen
                          spacemacs-dark
                          spacemacs-light
                          solarized-light
                          solarized-dark
                          leuven
-                         monokai
                          zenburn)
    ;; If non nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
