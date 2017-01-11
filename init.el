edf5000f00d107d3280756f6f82294a12bce17e9
Marco Orovecchia <marco.orovecchia@automic.com>
2017-01-11 15:19:53 +0100

    removed gnus layer

--- init.el
+++ init.el
@@ -64,7 +64,6 @@ values."
      windows-scripts
      (osx :variables osx-use-option-as-meta nil)
      yaml
-     gnus
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
